c2138b7c27b6fa583fa496dde2fc13972a7b3fbf
Dominique Alguacil <algu@zhaw.ch>
2026-06-15 15:37:58 +0200

    fixed db_connect

--- searchserv-db-init/db_connect.py
+++ searchserv-db-init/db_connect.py
@@ -5,17 +5,15 @@ import os
 # try to get a connection to the db going
 def db_connect():
     tries = 0
-    max_tries = int(os.getenv("DB_CONNECT_TRIES", "10"))
-    delay = float(os.getenv("DB_CONNECT_DELAY", "0.5"))
-    while tries < max_tries:
+    while tries < 100:
         try:
             conn = db.connect()
             break;
         except db.OperationalError as ex:
             print(ex)
             tries += 1
-            sleep(delay)
-    if tries == max_tries:
+            sleep(0.5)
+    if tries == 100:
         print("Couldn't connect to database, exiting")
         exit(1)
     curs = conn.cursor()
