b5c5854b532231e75a0c2ed2d08a7b991152504c
Dominique Alguacil <algu@zhaw.ch>
2026-06-15 15:42:49 +0200

    fixed db connect

--- searchserv/db_connect.py
+++ searchserv/db_connect.py
@@ -7,7 +7,7 @@ def db_connect():
     tries = 0
     while tries < 100:
         try:
-            conn = db.connect()
+            conn = db.connect("host=offlinekarte-search-db port=5432 user=postgres password=postgres")
             break;
         except db.OperationalError as ex:
             print(ex)
