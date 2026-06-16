8a31f7f262309ec5dff6c5577724d1d9b8727e75
Dominique Alguacil <algu@zhaw.ch>
2026-06-16 14:20:51 +0200

    added init and start scripts

---
+++ zskarte.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+set CMD=%1
+
+if "%CMD%"=="init" goto init
+if "%CMD%"=="start" goto start
+
+echo Usage: zskarte.bat init ^| start
+exit /b 1
+
+
+:init
+echo Running INIT workflow...
+
+docker compose -f mapserv-init/docker-compose.yml run --rm offlinekarte-tileserver-init
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+docker compose -f searchserv-db-init/docker-compose.yml build --no-cache searchserv-loader
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+docker compose -f searchserv-db-init/docker-compose.yml run --rm searchserv-loader
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+echo Init complete.
+exit /b 0
+
+
+:start
+echo Starting services...
+
+REM TODO: adjust to your real runtime setup
+docker compose up -d
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+echo Services started.
+exit /b 0
